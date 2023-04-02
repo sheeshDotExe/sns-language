a5a6bab0ebe06d91aac0c5eea8989f434cc8a3dc
sheeshDotExe <102514426+sheeshDotExe@users.noreply.github.com>
2023-04-02 13:36:16 +0200

    ok

--- interpreter/types/interpretType.c
+++ interpreter/types/interpretType.c
@@ -1,35 +1,43 @@
 #include "interpretType.h"
 
-int isString(char*value, unsigned int length){
-	if (contains(value, '"', length) || contains(value, '\'', length)){
+int isString(char *value, unsigned int length)
+{
+	if (contains(value, '"', length) || contains(value, '\'', length))
+	{
 		return 1;
 	}
 	return 0;
 }
 
-void fillTypes(struct CommonTypes* cTypes, int* types, unsigned int length){
+void fillTypes(struct CommonTypes *cTypes, int *types, unsigned int length)
+{
 	cTypes->length = length;
-	cTypes->codes = (int*)malloc(length*sizeof(int));
-	for (int i = 0; i < length; i++){
+	cTypes->codes = (int *)malloc(length * sizeof(int));
+	for (int i = 0; i < length; i++)
+	{
 		cTypes->codes[i] = types[i];
 	}
 }
 
-struct CommonTypes getValidTypes(char**value, unsigned int* lengthP){
+struct CommonTypes getValidTypes(char **value, unsigned int *lengthP)
+{
 	struct CommonTypes types;
 
 	int validTypes[NUMBER_OF_TYPES];
 	unsigned int length = 0;
 
-	if (*lengthP == 1){
+	if (*lengthP == 1)
+	{
 		validTypes[length] = Char_c;
 		length++;
 	}
 
 	validTypes[length] = String_c;
 	length++;
-	if (isString(*value, *lengthP)){
-		if (*lengthP == 3){
+	if (isString(*value, *lengthP))
+	{
+		if (*lengthP == 3)
+		{
 			validTypes[length] = Char_c;
 			length++;
 		}
@@ -38,10 +46,11 @@ struct CommonTypes getValidTypes(char**value, unsigned int* lengthP){
 	}
 
 	int Bool_v = stringToBool(*value);
-	if (Bool_v != -1){
+	if (Bool_v != -1)
+	{
 		*lengthP = 1;
 		free(*value);
-		*value = (char*)malloc(2*sizeof(char));
+		*value = (char *)malloc(2 * sizeof(char));
 		itoa(Bool_v, *value, 10);
 
 		validTypes[length] = Int_c;
@@ -53,12 +62,14 @@ struct CommonTypes getValidTypes(char**value, unsigned int* lengthP){
 		return types;
 	}
 
-	if (!isNum(*value, *lengthP)){
+	if (!isNum(*value, *lengthP))
+	{
 		validTypes[length] = Int_c;
 		length++;
 	}
 
-	if (!isFloat(*value, *lengthP)){
+	if (!isFloat(*value, *lengthP))
+	{
 		validTypes[length] = Float_c;
 		length++;
 	}
@@ -67,20 +78,18 @@ struct CommonTypes getValidTypes(char**value, unsigned int* lengthP){
 	return types;
 }
 
-struct Var* generateVarFromString(char*value, unsigned int length){
+struct Var *generateVarFromString(char *value, unsigned int length)
+{
 
-	//value will be eaten
+	// value will be eaten
 
-	char**valueP = &value;
+	char **valueP = &value;
 
 	struct CommonTypes types = getValidTypes(valueP, &length);
 
-	struct Var ret = generateVar(types.codes, types.length, "unnamed", *valueP, (struct Param*)NULL);
-
-	struct Var* var = (struct Var*)malloc(sizeof(struct Var));
-	*var = ret;
+	struct Var *ret = generateVar(types.codes, types.length, "unnamed", *valueP, (struct Param *)NULL);
 
 	free(types.codes);
 
-	return var;
+	return ret;
 }
