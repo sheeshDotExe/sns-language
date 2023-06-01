23d3a4cf06d6e958c8223a5ea4257650cc308b34
sheeshDotExe <102514426+sheeshDotExe@users.noreply.github.com>
2023-06-01 19:45:28 +0200

    new timeout

--- interpreter/httpParser/httpLibrary/httpLibrary.c
+++ interpreter/httpParser/httpLibrary/httpLibrary.c
@@ -73,7 +73,7 @@ struct Server* createServer(struct HeaderOptions* headerOptions){
 	unsigned int timeout = 1;
 	#else
 	struct timeval timeout;
-	timeout.tv_sec = 1;
+	timeout.tv_sec = 5;
 	timeout.tv_usec = 0;
 	#endif
 
